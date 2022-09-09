c01a5aceb3dfdc7aacb102c2ee163031803b49af
sebbycorp <sebastian.maniak@hashicorp.com>
2022-09-09 11:45:16 -0400

    updates

--- variables.tf
+++ variables.tf
@@ -1,19 +1,47 @@
-variable "pool_members" {}
-variable "tenant" {}
-variable "vip_address" {}
-variable "common_name" {}
-variable "pki_name" {}
-variable "as3tmpl" {}
+variable "pool_members" {
+    description = "List of pool members"
+    type        = list(string)
+}
+variable "tenant" {
+    description = "Tenant name"
+    type        = string
+}
+variable "vip_address" {
+    description = "VIP address"
+    type        = string
+}
+variable "common_name" {
+    description = "Common name"
+    type        = string
+}
+variable "pki_name" {
+    description = "PKI name"
+    type        = string
+}
+variable "as3tmpl" {
+    description = "AS3 template"
+    type        = string
+}
 variable "pool_members_port" {
-  default = "443"
+    description = "Pool members port"
+    type        = string
+    default     = "443"
 }
 variable "monitor" {
-  default = "https"
+    description = "Monitor"
+    type        = string
 }
 variable "load_balancing_mode" {
-  default = "round-robin"
+    description = "Load balancing mode"
+    type        = string
+    default = "round-robin"
 }
 variable "persistence_method" {
-  default = "cookie"
+    description = "Persistence method"
+    type        = string
+    default = "cookie"
 }
-variable "vault_pki_backend" {}
+variable "vault_pki_backend" {
+    description = "Vault PKI backend"
+    type        = string
+}
